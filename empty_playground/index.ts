42ff55dc15101230f2c8c25f55778d8f70eef618
Rainer Stropek <rainer@timecockpit.com>
2024-09-27 12:27:18 +0200

    Saved in TypeScript Playground at 2024-09-27T10:27:18.493Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,13 +1,13 @@
 function setup() {
     createCanvas(400, 400);
 
-}
+    const text1: string = 'Hello';
+    const text2: string = 'world';
+    
+    let text3 = `${text1} ${text2}`;
+    text3 = `${text3}!`;
+    text(text3, 50, 50);
 
-let x: number = 0;
-
-function draw() {
-    background("yellow");
-
-    x = x + 1;
-    circle(x, 150, 50)
+    const longText = 'ehfbsihbseaifsbfhesfhebsuhfd ehbfshfe dhsbfhse fdhsbhfbjs efdbshbs fhdsfbhs fshefbns fsehf dsf sehfdh hbsdhf dsh sdfh shfs fsdhf shdff sdhf k'
+    text(longText, 0, 200, 400, 400);
 }

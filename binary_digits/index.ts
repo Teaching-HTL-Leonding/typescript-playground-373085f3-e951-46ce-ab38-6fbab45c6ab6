e805ee6f543f260c954305fb7528c4074b77066c
Rainer Stropek <rainer@timecockpit.com>
2024-10-16 17:17:41 +0200

    Saved in TypeScript Playground at 2024-10-16T15:17:40.620Z

--- binary_digits/index.ts
+++ binary_digits/index.ts
@@ -2,9 +2,10 @@ function setup() {
   createCanvas(580, 400)
   background(0)
 
-  const num = Math.floor(random(0, 1_000_000));
-  const decimalNumber= num
-  const binaryString = "1010"
+  const num = Math.floor(random(0, 50));
+  const binaryString = num.toString(2).padStart(6, '0')
+  const xOffset = 90
+    const startX = 60
   
 
   noFill()
@@ -42,40 +43,8 @@ function setup() {
   noStroke();
   text(num, width / 2, height - 20);
 
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  text(num % 10, width / 1.13, height - 190,);
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  text(Math.floor(num / 10 % 10), width / 1.37, height - 190,);
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  text(Math.floor(num / 100 % 10), width / 1.73, height - 190,);
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  text(Math.floor(num / 1000 % 10), width / 2.35, height - 190,);
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  text(Math.floor(num / 10000 % 10), width / 3.7, height - 190,);
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  text(Math.floor(num / 100000 % 10), width / 8.5, height - 190,);
-    
+textSize(100)
+   for (let i = 0 ; i < binaryString.length ; i++) {
+     text(binaryString[i], startX + i * xOffset, height - 190);
+   }
 }

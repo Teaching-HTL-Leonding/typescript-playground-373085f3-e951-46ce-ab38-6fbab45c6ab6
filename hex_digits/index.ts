b4253167896d7f6e0aad1da76b57e12b86de365e
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 09:35:26 +0200

    Saved in TypeScript Playground at 2024-10-17T07:35:25.776Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -1,12 +1,8 @@
 function setup() {
-  createCanvas(300, 400)
+  createCanvas(320, 400)
   background(0)
 
-  const num = Math.floor(random(1000, 4000));
-  const hexString = num.toString(16).toUpperCase().padStart(2, '0')
-  const xOffset = 90
-    const startX = 60
-  
+  const num = Math.floor(random(0, 10000));
 
   noFill()
   strokeWeight(2)
@@ -16,20 +12,75 @@ function setup() {
   noFill()
   strokeWeight(2)
   stroke("yellow")
-  rect(120, 150, 70, 100)
+  rect(130, 150, 70, 100)
 
   noFill()
   strokeWeight(2)
   stroke("yellow")
-  rect(210, 150, 70, 100)
+  rect(230, 150, 70, 100)
 
   textAlign(CENTER, CENTER);
   fill("yellow");
   noStroke();
-  text(num, width / 2, height - 20);
+  text(num, width / 2, height - 20)
 
-textSize(100)
-   for (let num = 0; num < hexString.length; num++) {
-    text(hexString[num], startX + num * xOffset, height - 190);
-   }
-}
+  textAlign(CENTER, CENTER);
+  fill("yellow");
+  noStroke();
+  textSize(100)
+  const hexdigit1 = num %16
+  if(hexdigit1 === 10) {
+    text('A', width / 1.2, height - 190,)
+  } else if(hexdigit1 === 11) {
+    text('B', width / 1.2, height - 190,)
+  }  else if(hexdigit1 === 12) {
+    text('C', width / 1.2, height - 190,)
+  }  else if(hexdigit1 === 13) {
+    text('D', width / 1.2, height - 190,)
+  }  else if(hexdigit1 === 14) {
+    text('E', width / 1.2, height - 190,)
+  }  else if(hexdigit1 === 15) {
+    text('F', width / 1.2, height - 190,) }
+
+  text(hexdigit1, width / 1.2, height - 190,)
+
+
+  textAlign(CENTER, CENTER);
+  fill("yellow");
+  noStroke();
+  textSize(100)
+  const hexdigit2 =Math.floor(num/ 16) %16
+  if(hexdigit2 === 10) {
+    text('A', width / 1.2, height - 190,)
+  } else if(hexdigit2 === 11) {
+    text('B', width / 1.2, height - 190,)
+  }  else if(hexdigit2 === 12) {
+    text('C', width / 1.2, height - 190,)
+  }  else if(hexdigit2 === 13) {
+    text('D', width / 1.2, height - 190,)
+  }  else if(hexdigit2 === 14) {
+    text('E', width / 1.2, height - 190,)
+  }  else if(hexdigit2 === 15) {
+    text('F', width / 1.2, height - 190,) }
+
+  text(hexdigit2, width / 1.9, height - 190,)
+
+  textAlign(CENTER, CENTER);
+  fill("yellow");
+  noStroke();
+  textSize(100)
+   const hexdigit3 =Math.floor(num/ 256) %16
+  if(hexdigit3 === 10) {
+    text('A', width / 1.2, height - 190,)
+  } else if(hexdigit3 === 11) {
+    text('B', width / 1.2, height - 190,)
+  }  else if(hexdigit3 === 12) {
+    text('C', width / 1.2, height - 190,)
+  }  else if(hexdigit3 === 13) {
+    text('D', width / 1.2, height - 190,)
+  }  else if(hexdigit3 === 14) {
+    text('E', width / 1.2, height - 190,)
+  }  else if(hexdigit3 === 15) {
+    text('F', width / 1.2, height - 190,) }
+  text(hexdigit3, width / 4.9, height - 190,)
+  }

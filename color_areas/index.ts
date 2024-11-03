9fe36199eccbd204585112e5177c6148c14151ba
Rainer Stropek <rainer@timecockpit.com>
2024-11-03 14:42:15 +0100

    Saved in TypeScript Playground at 2024-11-03T13:42:15.213Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -1,34 +1,41 @@
 function setup() {
-    createCanvas(400, 200);
+    createCanvas(300, 200);
+    background("black");
+    noStroke();
+
+fill("gold")
+rect(0, 0, 100, 160)
+
+fill("green")
+rect(100, 0, 100, 160)
+
+fill("red")
+rect(200, 0, 100, 160)
+}
+
+
+function mouseClicked () {
+    createCanvas(300, 200);
     background("black");
     noStroke();
 
     fill("gold")
-    rect(0, 0, 135, 130)
+rect(0, 0, 100, 160)
 
-    fill("green")
-    rect(135, 0, 135, 130)
+fill("green")
+rect(100, 0, 100, 160)
 
-    fill("red")
-    rect(270, 0, 140, 130)
+fill("red")
+rect(200, 0, 100, 160)
+
+let message: string
+
+if(mouseX < width / 3) {
+    message = "Yellow"
+}else if(mouseX < width / 3 * 2) {
+    message = "Green"
+} else{message = "Red"}
+
+text(message, 150, 180)
+
 }
-
-function mouseClicked() {
-    let message: string
-
-    if (mouseX < width / 3) {
-        message = "Yellow"
-    } else if (mouseX < width / 3 * 2) {
-        message = "Green"
-    } else {
-        message = "Red"
-    }
-
-    fill("black")
-    rect(0, 130, 400, 200)
-
-    fill("white")
-    textSize(30)
-    text(message, 150, height - 25)
-}
-

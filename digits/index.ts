dfeb916bc55ff6f146e05e141c1d82a5a1110a67
Rainer Stropek <rainer@timecockpit.com>
2024-11-05 13:50:53 +0100

    Saved in TypeScript Playground at 2024-11-05T12:50:53.174Z

--- digits/index.ts
+++ digits/index.ts
@@ -2,7 +2,7 @@ function setup() {
   const num = Math.floor(random(0, 1_000_000));
   createCanvas(700, 250)
   background(0)
-  
+
   noFill()
   stroke("yellow")
   strokeWeight(2)
@@ -13,9 +13,18 @@ function setup() {
   rect(450, 50, 70, 100)
   rect(550, 50, 70, 100)
 
-  textSize(40)
-  text(num % 10, 60, 80)
-  
+  textSize(60)
+  fill("yellow")
+  strokeWeight(1)
+  text(num % 10, 570, 120)
+  text(Math.floor(num / 10) % 10, 470, 120)
+  text(Math.floor(num / 100) % 10, 370, 120)
+  text(Math.floor(num / 1000) % 10, 270, 120)
+  text(Math.floor(num / 10000) % 10, 170, 120)
+  text(Math.floor(num / 100000) % 10, 70, 120)
+
+
+
   textAlign(CENTER, CENTER);
   fill("yellow");
   noStroke();

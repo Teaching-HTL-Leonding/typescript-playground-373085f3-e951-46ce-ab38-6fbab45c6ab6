bf077b41dd17856312f754608cdc926a6d646bd5
Rainer Stropek <rainer@timecockpit.com>
2024-11-05 16:54:54 +0100

    Saved in TypeScript Playground at 2024-11-05T15:54:54.499Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -8,14 +8,8 @@ function setup() {
   strokeWeight(2)
   stroke("yellow")
   rect(30, 150, 70, 100)
-
-  noFill()
-  strokeWeight(2)
   stroke("yellow")
   rect(130, 150, 70, 100)
-
-  noFill()
-  strokeWeight(2)
   stroke("yellow")
   rect(230, 150, 70, 100)
 
@@ -24,63 +18,60 @@ function setup() {
   noStroke();
   text(num, width / 2, height - 20)
 
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  const hexdigit1 = num % 16
-  if (hexdigit1 === 10) {
-    text('A', width / 1.2, height - 190,)
-  } else if (hexdigit1 === 11) {
-    text('B', width / 1.2, height - 190,)
-  } else if (hexdigit1 === 12) {
-    text('C', width / 1.2, height - 190,)
-  } else if (hexdigit1 === 13) {
-    text('D', width / 1.2, height - 190,)
-  } else if (hexdigit1 === 14) {
-    text('E', width / 1.2, height - 190,)
-  } else if (hexdigit1 === 15) {
-    text('F', width / 1.2, height - 190,)
-  } else if (hexdigit1 < 10) { text(hexdigit1, width / 1.2, height - 190,) }
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  const hexdigit2 = Math.floor(num / 16) % 16
-  if (hexdigit2 === 10) {
-    text('A', width / 1.9, height - 190,)
-  } else if (hexdigit2 === 11) {
-    text('B', width / 1.9, height - 190,)
-  } else if (hexdigit2 === 12) {
-    text('C', width / 1.9, height - 190,)
-  } else if (hexdigit2 === 13) {
-    text('D', width / 1.9, height - 190,)
-  } else if (hexdigit2 === 14) {
-    text('E', width / 1.9, height - 190,)
-  } else if (hexdigit2 === 15) {
-    text('F', width / 1.9, height - 190,)
-  } else if (hexdigit2 < 10) { text(hexdigit2, width / 1.9, height - 190,) }
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(100)
-  const hexdigit3 = Math.floor(num / 256) % 16
-  if (hexdigit3 === 10) {
-    text('A', width / 4.9, height - 190,)
-  } else if (hexdigit3 === 11) {
-    text('B', width / 4.9, height - 190,)
-  } else if (hexdigit3 === 12) {
-    text('C', width / 4.9, height - 190,)
-  } else if (hexdigit3 === 13) {
-    text('D', width / 4.9, height - 190,)
-  } else if (hexdigit3 === 14) {
-    text('E', width / 4.9, height - 190,)
-  } else if (hexdigit3 === 15) {
-    text('F', width / 4.9, height - 190,)
+  textSize(50)
+  fill("Yellow")
+  let hex1 = num % 16
+  if (hex1 === 10) {
+    text('A', 60, 200)
+  } else if (hex1 === 11) {
+    text('B', 60, 200)
+  } else if (hex1 === 12) {
+    text('C', 60, 200)
+  } else if (hex1 === 13) {
+    text('D', 60, 200)
+  } else if (hex1 === 14) {
+    text('E', 60, 200)
+  } else if (hex1 === 15) {
+    text('F', 60, 200)
+  } else {
+    text(hex1, 60, 200)
   }
-  else if (hexdigit3 < 10) {
-    text(hexdigit3, width / 4.9, height - 190,)
+
+  textSize(50)
+  fill("Yellow")
+  let hex2 = Math.floor(num / 16) % 16
+  if (hex2 === 10) {
+    text('A', 160, 200)
+  } else if (hex2 === 11) {
+    text('B', 160, 200)
+  } else if (hex2 === 12) {
+    text('C', 160, 200)
+  } else if (hex2 === 13) {
+    text('D', 160, 200)
+  } else if (hex2 === 14) {
+    text('E', 160, 200)
+  } else if (hex2 === 15) {
+    text('F', 160, 200)
+  } else {
+    text(hex2, 160, 200)
+  }
+
+  textSize(50)
+  fill("Yellow")
+  let hex3 = Math.floor(num /256) % 16
+  if (hex3 === 10) {
+    text('A', 260, 200)
+  } else if (hex3 === 11) {
+    text('B', 260, 200)
+  } else if (hex3 === 12) {
+    text('C', 260, 200)
+  } else if (hex3 === 13) {
+    text('D', 260, 200)
+  } else if (hex3 === 14) {
+    text('E', 260, 200)
+  } else if (hex3 === 15) {
+    text('F', 260, 200)
+  } else {
+    text(hex3, 260, 200)
   }
 }

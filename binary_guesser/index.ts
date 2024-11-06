467bc546aabd3db78bbb9bdaa9674df57f222518
Rainer Stropek <rainer@timecockpit.com>
2024-11-06 14:39:21 +0100

    Saved in TypeScript Playground at 2024-11-06T13:39:20.631Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -19,36 +19,62 @@ function setup() {
   strokeWeight(2)
   stroke("yellow")
   rect(30, 100, 70, 100)
-  stroke("yellow")
   rect(120, 100, 70, 100)
-  stroke("yellow")
   rect(210, 100, 70, 100)
-  stroke("yellow")
   rect(300, 100, 70, 100)
 
   fill("Yellow")
   text(num4, 53, 165)
-  text(num3,143, 165)
+  text(num3, 143, 165)
   text(num2, 233, 165)
   text(num1, 323, 165)
 }
 
 function mouseClicked() {
-if(mouseX > 30 && mouseX <= 100 && mouseY > 100 && mouseY <= 200) {
-  if(num4 === 1){
-    num4 = 0
-  }else{ num4 = 1}
-}if(mouseX > 120 && mouseX <= 190 && mouseY > 100 && mouseY <= 200) {
-    if(num3 === 1){
-    num3 = 0
-  }else{ num3 = 1}
-}if(mouseX > 210 && mouseX <= 280 && mouseY > 100 && mouseY <= 200) {
-    if(num2 === 1){
-    num2 = 0
-  }else{ num2 = 1}
-}if(mouseX > 300 && mouseX <= 370 && mouseY > 100 && mouseY <= 200) {
-    if(num1 === 1){
-    num1 = 0
-  }else{ num1 = 1}
-}
+  if (mouseX > 30 && mouseX <= 100 && mouseY > 100 && mouseY <= 200) {
+    if (num4 === 1) {
+      num4 = 0
+    } else { num4 = 1 }
+  } if (mouseX > 120 && mouseX <= 190 && mouseY > 100 && mouseY <= 200) {
+    if (num3 === 1) {
+      num3 = 0
+    } else { num3 = 1 }
+  } if (mouseX > 210 && mouseX <= 280 && mouseY > 100 && mouseY <= 200) {
+    if (num2 === 1) {
+      num2 = 0
+    } else { num2 = 1 }
+  } if (mouseX > 300 && mouseX <= 370 && mouseY > 100 && mouseY <= 200) {
+    if (num1 === 1) {
+      num1 = 0
+    } else { num1 = 1 }
+  }
+
+  let guessed = num4 * Math.pow(2, 0)
+  guessed += num3 * Math.pow(2, 1)
+  guessed += num2 * Math.pow(2, 2)
+  guessed += num1 * Math.pow(2, 3)
+
+  let color = "yellow"
+  if (guessed === num) {
+    color = "lightgreen"
+
+    textSize(50)
+    fill("lightgreen")
+    text("Correct", width / 2 - 90, 270)
+  }
+
+  fill(0)
+  strokeWeight(2)
+  stroke(color)
+  rect(30, 100, 70, 100)
+  rect(120, 100, 70, 100)
+  rect(210, 100, 70, 100)
+  rect(300, 100, 70, 100)
+
+  fill(color)
+  noStroke()
+  text(num4, 53, 165)
+  text(num3, 143, 165)
+  text(num2, 233, 165)
+  text(num1, 323, 165)
 }

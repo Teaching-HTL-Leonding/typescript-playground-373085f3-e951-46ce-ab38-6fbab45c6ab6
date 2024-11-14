3ab4ea119f7f842e45645b60ef7419a9dbb401fe
Rainer Stropek <rainer@timecockpit.com>
2024-11-14 08:50:38 +0100

    Saved in TypeScript Playground at 2024-11-14T07:50:38.065Z

--- rays/index.ts
+++ rays/index.ts
@@ -12,26 +12,26 @@ function mouseMoved() {
   strokeWeight(2);
 
 let y = MARGIN
-while(y < 375){
+while(y <= 375){
   stroke("yellow")
  line(MARGIN, y, mouseX, mouseY)
  y += MARGIN
 }
 y = MARGIN
-while(y < 375){
+while(y <= 375){
   stroke("yellow")
  line(375, y, mouseX, mouseY)
  y += MARGIN
 }
 let x = MARGIN
-while(x < 375){
-  stroke("yellow")
+while(x <= 375){
+  stroke("lightgreen")
  line(x, 375, mouseX, mouseY)
  x += MARGIN
 }
-x=MARGIN
-while(x < 375){
-  stroke("yellow")
+x = MARGIN
+while(x <= 375){
+  stroke("lightgreen")
  line(x, MARGIN, mouseX, mouseY)
  x += MARGIN
 }

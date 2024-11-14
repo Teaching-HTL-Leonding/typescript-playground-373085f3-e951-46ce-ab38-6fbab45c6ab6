02dd42381c7358b1e8bbdf4064385812f7ecc254
Rainer Stropek <rainer@timecockpit.com>
2024-11-14 10:08:05 +0100

    Saved in TypeScript Playground at 2024-11-14T09:08:05.251Z

--- zebra_stripes/index.ts
+++ zebra_stripes/index.ts
@@ -12,7 +12,7 @@ while(xy <= SIZE){
     fill("yellow")
     rect(0 ,xy, SIZE, STRIPE_THICKNESS)
     fill("lime")
-    rect(0, xy * 2, SIZE, STRIPE_THICKNESS)
+    rect(0, xy + STRIPE_THICKNESS, SIZE, STRIPE_THICKNESS)
 
     xy += STRIPE_THICKNESS
 }

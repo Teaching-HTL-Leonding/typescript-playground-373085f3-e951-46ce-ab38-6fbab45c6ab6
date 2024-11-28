e9bc362ff651cea0f4ad929f315632af8c1215c8
Rainer Stropek <rainer@timecockpit.com>
2024-11-28 09:38:58 +0100

    Saved in TypeScript Playground at 2024-11-28T08:38:57.923Z

--- triangles/index.ts
+++ triangles/index.ts
@@ -12,9 +12,11 @@ function setup() {
     strokeWeight(1);
     stroke("yellow");
     noFill();
+    for(let y = HEIGHT; y <= HEIGHT * 9; y += HEIGHT){
     for (let x = 0; x <= SIZE; x += SIDE_LENGTH) {
-        triangle(x, SIDE_LENGTH, SIDE_LENGTH / 2 + x, 0, x + SIDE_LENGTH, SIDE_LENGTH)
+        triangle(x, HEIGHT, SIDE_LENGTH / 2 + x, 0, x + SIDE_LENGTH, HEIGHT)
     
+    }
     }
 }
 

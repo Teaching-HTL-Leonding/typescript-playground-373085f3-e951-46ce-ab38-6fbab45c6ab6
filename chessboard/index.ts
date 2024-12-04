9d044c8a04f9da164d8382be5d7c5812e18f17ec
Rainer Stropek <rainer@timecockpit.com>
2024-12-04 10:50:27 +0100

    Saved in TypeScript Playground at 2024-12-04T09:50:27.343Z

--- chessboard/index.ts
+++ chessboard/index.ts
@@ -11,16 +11,6 @@ function setup() {
         for (let x = 0; x <= SIZE; x += SQUARE_SIZE) {
             fill("brown")
             rect(x, y, SQUARE_SIZE, SQUARE_SIZE)
-            if (x === 50 || 150 || 250 || 350 || 450) {
-                fill("white")
-                rect(x, y, SQUARE_SIZE, SQUARE_SIZE)
-            } else if (y === 50 || 150 || 250 || 350 || 450) {
-                fill("white")
-                rect(x, y, SQUARE_SIZE, SQUARE_SIZE)
-            } else {
-                fill("brown")
-                rect(x, y, SQUARE_SIZE, SQUARE_SIZE)
-            }
         }
     }
 }

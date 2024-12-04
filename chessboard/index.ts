c4962446256f7fb9872e3fd9d930262709e85061
Rainer Stropek <rainer@timecockpit.com>
2024-12-04 18:32:57 +0100

    Saved in TypeScript Playground at 2024-12-04T17:32:56.887Z

--- chessboard/index.ts
+++ chessboard/index.ts
@@ -7,10 +7,36 @@ function setup() {
 
     noStroke();
 
-    for (let y = 0; y <= SIZE; y += SQUARE_SIZE) {
-        for (let x = 0; x <= SIZE; x += SQUARE_SIZE) {
+    for (let y = 0; y < 64; y++) {
+        let x = Math.floor(y / 8)
+        let z = y % 8
+        if (x % 2 === 0 && z % 2 === 1 || z % 2 === 0 && x % 2 === 1) {
             fill("brown")
-            rect(x, y, SQUARE_SIZE, SQUARE_SIZE)
+        } else {
+            fill("white")
         }
+        rect(SQUARE_SIZE + z * SQUARE_SIZE, SQUARE_SIZE + x * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+    
+    textSize(20)
+    textAlign(CENTER,CENTER)
+    
+    for(let i = 1; i <= 8; i ++){
+       text(i, SQUARE_SIZE / 2, height - SQUARE_SIZE * i - SQUARE_SIZE / 2)
+    let file: string
+    switch(i){
+        case 1: file = "a"; break;
+        case 2: file = "b"; break;
+        case 3: file = "c"; break;
+        case 4: file = "d"; break;
+        case 5: file = "e"; break;
+        case 6: file =" f"; break;
+        case 7: file = "g"; break;
+        case 8: file = "h"; break;
     }
+
+    
+  text(file, SQUARE_SIZE * i + SQUARE_SIZE / 2, height - SQUARE_SIZE / 2)
+   
+    }
+}
 }

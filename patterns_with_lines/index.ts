4192395404401d79c12ab9906ccc9f76f242c71a
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 09:01:49 +0100

    Saved in TypeScript Playground at 2024-12-05T08:01:49.022Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -11,15 +11,26 @@ function setup() {
 }
 
 function mouseMoved() {
+    colorMode(HSB)
     resetMatrix();
     background("black");
     strokeWeight(2);
 
     noFill();
     stroke("aqua");
-    for(let x = 0; )
-    for(let i = 0; i <= 250; i += 10){
-        line(0, 0 + i, 5, 5 + i)
-        line(5 , 5 + i, 0, 10 + i)
+
+    for (let x = 0; x < 601; x += 10) {
+        for (let i = 0; i <= 301; i += 5) {
+            if(x >= 360){
+                x === 0
+            }
+            stroke(x + 60, x + 60, x)
+            line(x, 0, x + 10, 5 + i)
+            line(x + 10, 5 + i, x, i + 10)
+        }
     }
+
+noStroke()
+    fill(0)
+    rect(0, 280, 601, 21)
 }

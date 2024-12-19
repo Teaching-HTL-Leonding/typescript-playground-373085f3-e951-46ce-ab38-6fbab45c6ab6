fbcc0494e54f6893e3b4148c0b5a94070fd91bc2
Rainer Stropek <rainer@timecockpit.com>
2024-12-19 08:45:08 +0100

    Saved in TypeScript Playground at 2024-12-19T07:45:07.895Z

---
+++ word_swirrel/index.ts
@@ -0,0 +1,41 @@
+const WIDTH = 500;
+const HEIGHT = 300;
+
+// Enter the word to guess here.
+//                   vvv
+//                   vvv
+const wordToGuess = "apple";
+
+function setup() {
+  createCanvas(WIDTH, HEIGHT);
+  background("black");
+
+  let wordToScramble = wordToGuess;
+  let scrambleWord = "";
+  while(wordToScramble.length > 0){
+    let letterIndex = Math.floor(random(wordToScramble.length));
+    scrambleWord += wordToScramble[letterIndex];
+    wordToScramble = wordToScramble.substring(0,letterIndex)
+     + wordToScramble.substring(letterIndex + 1)
+  }
+
+  textAlign(CENTER,CENTER)
+  fill("white")
+  textSize(75)
+  text(scrambleWord, WIDTH / 2, HEIGHT / 2)
+}
+
+// This method will be called automatically when the user clicks "Guess".
+// The guessed text will be in "textInput".
+function guess(textInput: string) {
+fill("white")
+if(textInput === wordToGuess) {
+  background("green")
+  textSize(75)
+  text("correct!", WIDTH / 2, HEIGHT / 2)
+} else {
+  background("red")
+  textSize(75)
+  text(`Wrong!\nlt was "${wordToGuess}"`, WIDTH / 2, HEIGHT / 2)
+}
+}

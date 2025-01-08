43f3604f5def747eb5ff29ca7bf5a8b6098f6ad1
Rainer Stropek <rainer@timecockpit.com>
2025-01-08 16:32:14 +0100

    Saved in TypeScript Playground at 2025-01-08T15:32:13.924Z

--- parse_coordinates/index.ts
+++ parse_coordinates/index.ts
@@ -1,5 +1,3 @@
-// The following string represents coordinates (x,y) of circles
-// that you have to draw. Each circle must have a radius of 40.
 const circles = "50,50 50,100 100,50 100,100";
 
 function setup() {
@@ -12,25 +10,23 @@ function setup() {
     let x = 0
     let y = 0
     let co = ""
-    for(let ix = 0; ix < circles.length; ix ++){
-        switch(circles[ix]){
-            case ",": 
-            x = parseInt(co)
-            co = ""
-            break;
-
+    for (let ix = 0; ix < circles.length; ix++) {
+        switch (circles[ix]) {
+            case ",":
+                x = parseInt(co)
+                co = ""
+                break;
             case "":
-            y = parseInt(co)
-            co = ""
-            break;
-
+                y = parseInt(co)
+                circle(x, y, 40)
+                co = ""
+                break;
             default:
-
-            co += circles[ix]
-            break;
+                co += circles[ix]
+                break;
         }
-
-        y = parseInt(co)
-        circle(x, y, 40)
     }
+
+    y = parseInt(co)
+    circle(x, y, 40)
 }

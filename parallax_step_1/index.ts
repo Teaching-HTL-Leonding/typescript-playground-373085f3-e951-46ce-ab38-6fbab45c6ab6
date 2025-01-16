54ac243d84520dd18320ab50eac443523b2d1044
Rainer Stropek <rainer@timecockpit.com>
2025-01-16 09:46:00 +0100

    Saved in TypeScript Playground at 2025-01-16T08:46:00.308Z

--- parallax_step_1/index.ts
+++ parallax_step_1/index.ts
@@ -1,4 +1,8 @@
 let backgroundImages: p5.Image[] = [];
+let backgroundscale: number = 1
+let scaledImagesWidth: number = 0
+let scaledImagesHeight: number = 0
+
 
 function preload() {
     for (let i = 0; i < winter.length; i++) {
@@ -8,9 +12,13 @@ function preload() {
 
 function setup() {
     createCanvas(500, 500)
+
+    backgroundscale = width / backgroundImages[0].width;
+    scaledImagesWidth = backgroundImages[0].width * backgroundscale
+    scaledImagesHeight = backgroundImages[0].height * backgroundscale
 }
 
 function draw() {
     for (let i = 0; i < backgroundImages.length; i++)
-        image(backgroundImages[i], 0, 0, 500, 500)
+        image(backgroundImages[i], 0, 0, scaledImagesWidth, scaledImagesHeight)
 }

3865816edcff5ce6a6ae3160abca6c168e087094
Rainer Stropek <rainer@timecockpit.com>
2025-01-16 10:25:24 +0100

    Saved in TypeScript Playground at 2025-01-16T09:25:24.242Z

--- parallax_step_2/index.ts
+++ parallax_step_2/index.ts
@@ -1,75 +1,36 @@
-const THEME_IX = 2;
+let backgroundImages: p5.Image[] = [];
+let backgroundscale: number = 1
+let scaledImagesWidth: number = 0
+let scaledImagesHeight: number = 0
+let scrollposition: number = 0
 
-const images: p5.Image[] = [];
-
-let backgroundScale = 1;
-let scaledImageWidth = 0;
-let scaledImageHeight = 0;
-
-// Number of pixels to scroll when the user presses the cursor keys
-const SCROLL_SPEED = 5;
-
-// Current scroll position in x direction. A positive value means that the background
-// images are moved to the right. A negative value means that the background images are moved to the left.
-let scrollPosition = 0;
 
 function preload() {
-    // Same code as in the previous example
-
-    const theme = themes[THEME_IX];
-    for (let i = 0; i < theme.length; i++) {
-        const imageName = theme[i];
-        const image = loadImage(`${BASE_URL}/${themeNames[THEME_IX]}/${imageName}`);
-        images.push(image);
+    for (let i = 0; i < winter.length; i++) {
+        backgroundImages.push(loadImage(`${BASE_URL}/winter/${winter[i]}`));
     }
 }
 
 function setup() {
-    // Same code as in the previous example
+    createCanvas(500, 500)
 
-    createCanvas(500, 500);
-
-    backgroundScale = width / images[0].width;
-    scaledImageWidth = images[0].width * backgroundScale;
-    scaledImageHeight = images[0].height * backgroundScale;
+    backgroundscale = width / backgroundImages[0].width;
+    scaledImagesWidth = backgroundImages[0].width * backgroundscale
+    scaledImagesHeight = backgroundImages[0].height * backgroundscale
 }
 
 function draw() {
-    background("black");
+    background(0)
 
-    // Check if user presses the cursor keys
-    if (keyIsDown(39)) {
-        // Cursor right
-        scrollPosition -= SCROLL_SPEED;
-    } else if (keyIsDown(37)) {
-        // Cursor left
-        scrollPosition += SCROLL_SPEED;
+    if (keyIsDown(37)) {
+        scrollposition -= 5
+    } else if (keyIsDown(39)) {
+        scrollposition += 5
     }
 
-    // Uncomment the following lines to zoom out. This will make it easier for you
-    // to see how the background images move out of the visible area when you press the cursor keys.
-    scale(0.5);
-    translate(width / 2, height / 2);
-
-    push();
-
-    // Translate the background images according to the current scroll position
-    translate(scrollPosition, 0);
-
-    // From here on, the code is the same as in the previous example
-    translate(0, (height - scaledImageHeight) / 2);
-    for (let i = 0; i < images.length; i++) {
-        const img = images[i];
-        image(img, 0, 0, scaledImageWidth, scaledImageHeight);
-    }
-    pop();
-
-    // Uncomment the following lines to draw a rectangle around the canvas. This will help you to see
-    // how the background images move out of the visible area when you press the cursor keys. Use
-    // these lines of code together with the commented _scale()_ and _translate()_ functions above.
-    push();
-    noFill();
-    stroke("red");
-    rect(0, 0, width, height);
-    pop();
+    translate(scrollposition, 0)
+    for (let i = 0; i < backgroundImages.length; i++)
+    image(backgroundImages[i], -scaledImagesWidth, 0, scaledImagesWidth, scaledImagesHeight)
+    image(backgroundImages[i], 0, 0, scaledImagesWidth, scaledImagesHeight)
+    image(backgroundImages[i], scaledImagesWidth, 0, scaledImagesWidth, scaledImagesHeight)
 }

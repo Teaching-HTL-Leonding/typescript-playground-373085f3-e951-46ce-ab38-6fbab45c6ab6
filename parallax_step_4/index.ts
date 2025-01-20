ef5324009a85387fac48076990fe73dffd35a236
Rainer Stropek <rainer@timecockpit.com>
2025-01-20 09:45:27 +0100

    Saved in TypeScript Playground at 2025-01-20T08:45:27.089Z

--- parallax_step_4/index.ts
+++ parallax_step_4/index.ts
@@ -4,15 +4,58 @@ let scaledImagesWidth: number = 0
 let scaledImagesHeight: number = 0
 let scrollposition: number = 0
 
-let figurImage: p5.Image[] = []
+const BASE_URL_worm = "https://cddataexchange.blob.core.windows.net/images/parallax/worm/"
+
+const WORM = [
+    "Moving_01.png",    
+    "Moving_02.png",    
+    "Moving_03.png",    
+    "Moving_04.png",    
+    "Moving_05.png",    
+    "Moving_06.png",    
+    "Moving_07.png",    
+    "Moving_08.png",    
+    "Moving_09.png",    
+    "Moving_10.png",    
+    "Moving_11.png",    
+    "Moving_12.png",    
+    "Moving_13.png",    
+    "Moving_14.png",    
+    "Moving_15.png",    
+    "Moving_16.png",    
+    "Moving_17.png",    
+    "Moving_18.png",    
+    "Moving_19.png",    
+    "Moving_20.png",    
+    "Moving_21.png",    
+    "Moving_22.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",    
+    "Moving_00.png",
+]
+
+let figurImages: p5.Image[] = []
 let figurscale: number = 1
 
 function preload() {
     for (let i = 0; i < winter.length; i++) {
         backgroundImages.push(loadImage(`${BASE_URL}/winter/${winter[i]}`));
-    }
-    for(let a = 0; a < figurImage.length; a ++){
-        figurImage.push(loadImage())
+        figurImages.push(loadImage(`${BASE_URL_worm}`))
     }
 }
 

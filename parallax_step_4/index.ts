90b40fb2412a49010c8e4af23bd1175af938dc76
Rainer Stropek <rainer@timecockpit.com>
2025-01-22 10:51:42 +0100

    Saved in TypeScript Playground at 2025-01-22T09:51:42.093Z

--- parallax_step_4/index.ts
+++ parallax_step_4/index.ts
@@ -1,3 +1,6 @@
+let backgroundImages: p5.Image[] = [];
+let backgroundscale: number = 1
+let scaledImagesWidth: number = 0
 let scaledImagesHeight: number = 0
 let scrollposition: number = 0
 
@@ -49,12 +52,38 @@ const WORM = [
 let figurImages: p5.Image[] = []
 let figurscale: number = 1
 
+
 function preload() {
     for (let i = 0; i < winter.length; i++) {
         backgroundImages.push(loadImage(`${BASE_URL}/winter/${winter[i]}`));
     }
     for(let a = 0; a < figurImage.length; a ++){
-        figurImage.push(loadImage())
-        figurImages.push(loadImage(`${BASE_URL_worm}`))
+        figurImages.push(loadImage(`${BASE_URL_worm}/worm/${WORM[a]}`))
     }
+}
+
+function setup() {
+    createCanvas(500, 500)
+
+    backgroundscale = width / backgroundImages[0].width;
+    scaledImagesWidth = backgroundImages[0].width * backgroundscale
+    scaledImagesHeight = backgroundImages[0].height * backgroundscale
+}
+
+function draw() {
+    background(0)
+
+    if (keyIsDown(37)) {
+        scrollposition -= 5
+    } else if (keyIsDown(39)) {
+        scrollposition += 5
+    }
+
+    translate(scrollposition, 0)
+    for (let i = 0; i < backgroundImages.length; i++){
+    image(backgroundImages[i], -scaledImagesWidth, 0, scaledImagesWidth, scaledImagesHeight)
+    image(backgroundImages[i], 0, 0, scaledImagesWidth, scaledImagesHeight)
+    image(backgroundImages[i], scaledImagesWidth, scaledImagesHeight)q1
+    }
+
 }

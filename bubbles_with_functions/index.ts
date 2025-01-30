c597c0b8d4cd32e185d76a2a6d166696cee13a97
Rainer Stropek <rainer@timecockpit.com>
2025-01-30 09:47:20 +0100

    Saved in TypeScript Playground at 2025-01-30T08:47:19.579Z

--- bubbles_with_functions/index.ts
+++ bubbles_with_functions/index.ts
@@ -4,20 +4,14 @@ const circles_diameter: number[] = [];
 
 let waiting_time = 3000;
 let circle_interval: number;
-let onecricle_invertal: number
 let level_interval: number;
 
 let points = 0;
 
 function setup() {
   createCanvas(300, 300);
-  onecricle_invertal = setInterval(addRandomCircle, 0)
   circle_interval = setInterval(addRandomCircle, waiting_time)
-  if(circle_interval){
-  circles_x.push(random(0, 300))
-  circles_y.push(random(0, 300))
-  circles_diameter.push(Math.floor(random(10, 50)))
-  }
+
 }
 
 function draw() {

cafb86df3e02e12e1938ac5aa665d106741eec35
Rainer Stropek <rainer@timecockpit.com>
2025-02-05 08:47:42 +0100

    Saved in TypeScript Playground at 2025-02-05T07:47:42.202Z

--- bubbles_with_functions/index.ts
+++ bubbles_with_functions/index.ts
@@ -1,6 +1,9 @@
 const circles_x: number[] = [];
 const circles_y: number[] = [];
 const circles_diameter: number[] = [];
+const xx: number[] = []
+const yy: number[] = []
+const dd: number[] = []
 
 let waiting_time = 3000;
 let circle_interval: number;
@@ -11,10 +14,11 @@ let points = 0;
 function setup() {
   createCanvas(300, 300);
   circle_interval = setInterval(addRandomCircle, waiting_time)
-
+  circle_interval = setInterval(addOneCircle, 0)
 }
 
 function draw() {
+
   push()
   background("black");
   for (let i = 0; i < circles_x.length; i++) {
@@ -22,6 +26,12 @@ function draw() {
     stroke("green")
     strokeWeight(2)
     circle(circles_x[i], circles_y[i], circles_diameter[i])
+  }
+  for (let i = 0; i <= 0; i++) {
+    noFill()
+    stroke("green")
+    strokeWeight(2)
+    circle(xx[i], yy[i], dd[i])
   }
   pop()
 
@@ -31,4 +41,14 @@ function addRandomCircle() {
   circles_x.push(random(0, 300))
   circles_y.push(random(0, 300))
   circles_diameter.push(Math.floor(random(10, 50)))
+}
+
+function addOneCircle() {
+  xx.push(Math.floor(random(0, 300)))
+  yy.push(Math.floor(random(0, 300)))
+  dd.push(Math.floor(random(10, 50)))
+}
+
+function isInside(){
+
 }

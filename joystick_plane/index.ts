559d05aee002cb306083ae6446e140a3294fb9f5
Rainer Stropek <rainer@timecockpit.com>
2025-03-27 09:50:32 +0000

    Saved in TypeScript Playground at 2025-03-27T09:50:31.824Z

--- joystick_plane/index.ts
+++ joystick_plane/index.ts
@@ -12,6 +12,7 @@ let y = 450
 
 let isDragging = false
 
+
 function preload() {
   fighter = loadImage("https://cddataexchange.blob.core.windows.net/images/Spaceship.png");
 }
@@ -19,17 +20,32 @@ function preload() {
 function setup() {
   createCanvas(500, 500);
 
- 
+
 }
 
 function draw() {
   background("lightblue");
+  const speedx = (x - 250) / 5
+  const speedy = (y - 450) / 5
+  fighterPositionX += speedx
+  fighterPositionY += speedy
+
+  if(fighterPositionX <= 0 ){
+    fighterPositionX = 0
+  } else if(fighterPositionX >= 500 ){
+    fighterPositionX = 500
+  } else  if(fighterPositionY <= 0 ){
+    fighterPositionY = 0
+  } else  if(fighterPositionY >= 500 ){
+    fighterPositionY = 500
+  }
 
   push();
   imageMode(CENTER);
   translate(width / 2 + fighterPositionX, height / 2 + fighterPositionY);
   image(fighter, 0, 0, fighterDisplayWidth, fighterDisplayHeight);
   pop();
+
 
   stroke("red")
   fill("white")
@@ -69,4 +85,4 @@ function phytagoras(): boolean {
   if (distance <= 10) {
     return true
   }
-}
+}    
